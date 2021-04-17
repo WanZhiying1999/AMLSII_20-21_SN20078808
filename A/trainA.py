ec2789183af4efe2807deac52c0a130c3b011037
Zhiying Wan <72938781+WanZhiying1999@users.noreply.github.com>
2021-04-17 23:05:05 +0100

    Update and rename train.py to trainA.py

--- A/trainA.py
+++ A/trainA.py
@@ -7,15 +7,14 @@ from keras.utils.np_utils import*
 import keras
 from keras import Sequential
 from keras.models import Model
-from keras.layers import Dense, Dropout, LSTM, Bidirectional
+from keras.layers import Dense, Dropout, LSTM, Bidirectional, Conv1D, MaxPooling1D, Flatten, Activation
 from keras.layers.embeddings import Embedding
 import matplotlib.pyplot as plt
-
-#use  pre-trained GloVe(Global vectors for words representations) mdoel to realize word embeddings
+#use  pre-trained GloVe(Global vectors for words representations) model to realize word embeddings
 # read glove file and output three dictionaries
-# * word_to_index: a dictionary mapping from words to their indices in the vocabulary
-# * index_to_word: dictionary mapping from indices to their corresponding words in the vocabulary
-# * word_to_vec_map: dictionary mapping words to their GloVe vector representation 
+# word_to_index: a dictionary mapping from words to their indices in the vocabulary
+# index_to_word: dictionary mapping from indices to their corresponding words in the vocabulary
+# word_to_vec: dictionary mapping words to their GloVe vector representation
 def read_glove_vecs(glove_file):
     with open(glove_file, 'r', encoding="utf8") as f:
         words = set()
@@ -63,7 +62,7 @@ def sentence_to_indices(sentence_words, word_to_index, max_len, i):
 
 def pretrained_embedding_layer(word_to_vec_map, word_to_index, max_len):
     vocab_len = len(word_to_index) + 1
-    emb_dim = word_to_vec_map["unk"].shape[0] #
+    emb_dim = word_to_vec_map["unk"].shape[0]
     
     emb_matrix = np.zeros((vocab_len, emb_dim))
     
@@ -76,22 +75,17 @@ def pretrained_embedding_layer(word_to_vec_map, word_to_index, max_len):
     
     return embedding_layer
 
+def train_A(Clean_trainA):
 
-def A_train(Clean_trainA):
     #load tidy data
     data = pd.read_csv(Clean_trainA)
     #remain label and tidy tweet
     data = data[['Sentiment','tidy_tweet']]
-    #df_pos = data[data['Sentiment'] == 'positive']
-    #df_neg = data[data['Sentiment'] == 'negative']
-    #df_neu = data[data['Sentiment'] == 'neutral']
-    #print(len(df_pos), len(df_neg),len(df_neu))
-
     #creat dataset and tokenization
     tk = TweetTokenizer(reduce_len=True)
-    global X, Y
-    X = []
-    Y = []
+    global X,Y
+    X=[]
+    Y=[]
     tidy_token_list = []
     for x, y in zip(data['tidy_tweet'], data['Sentiment']):
         x=json.dumps(x)
@@ -101,12 +95,8 @@ def A_train(Clean_trainA):
 
     #print(tidy_token_list)
     word_to_index, index_to_word, word_to_vec_map = read_glove_vecs('model\\glove.6B.100d.txt') 
-    #data padding 
-    #calculate the length of the longest phrase
-    
 
-    #list_len = [len(i) for i, j in tidy_token_list]
-    #max_len = max(list_len)
+
     max_len = 30
     print('max_len:', max_len)
 
@@ -120,30 +110,35 @@ def A_train(Clean_trainA):
 
     Y = to_categorical(Y, 3)
 
-    #print(X[:1])
-    #print(Y[:5])
-
-    #X_train, Y_train, X_val, Y_val = train_test_split(X, Y, test_size=0.2, random_state=0, stratify=Y)
-    #build the model
-    #embedding layer
-
 
     model = Sequential()
 
     model.add(pretrained_embedding_layer(word_to_vec_map, word_to_index, max_len))
-    model.add(Dropout(rate=0.4))
-    model.add(Bidirectional(LSTM(units=128, return_sequences=True)))
-    model.add(Dropout(rate=0.4))
-    model.add(Bidirectional(LSTM(units=128, return_sequences=False)))
-    model.add(Dense(units=3, activation='softmax'))
+    
+    #model.add(Conv1D(filters, kernel_size, padding='valid', activation='relu', strides=1))
+    model.add(Conv1D(256, 2, padding='same', activation='relu', strides=1))
+    model.add(Dropout(0.4))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(256, 3, padding='same', activation='relu', strides=1))
+    model.add(Dropout(0.4))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Conv1D(256, 4, padding='same', activation='relu', strides=1))
+    model.add(Dropout(0.4))
+    model.add(MaxPooling1D(pool_size=2))
+    model.add(Bidirectional(LSTM(units=256, return_sequences=True)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(300))
+
+    model.add(Dense(3, activation='softmax'))
+
 
     model.summary()
 
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    H = model.fit(X, Y, validation_split=0.2, epochs = 10, batch_size = 32, shuffle=True)
+    H = model.fit(X, Y, validation_split=0.2, epochs = 9, batch_size = 128, shuffle=True)
     model.save('A\\modelA.hdf5')
-
     plt.plot(H.history['acc'])
     plt.plot(H.history['val_acc'])
     plt.title('Model A accuracy')
@@ -151,18 +146,22 @@ def A_train(Clean_trainA):
     plt.xlabel('Epoch')
     plt.legend(['Train','Validation'],loc='upper left')
     plt.show()
-    plt.savefig("Accuracy.png")
+    plt.savefig("Accuracy_A.png")
     plt.plot(H.history['loss'])
     plt.plot(H.history['val_loss'])
     plt.title('Model A loss')
     plt.ylabel('loss')
     plt.xlabel('Epoch')
-    plt.legend(['Train','Validation'],loc='upper left')
+    plt.legend(['Train','Validation'],loc='upper right')
     plt.show()
+    plt.savefig("Loss_A.png")
 
 
     return H.history['acc'][-1], H.history['val_acc'][-1]
 
+#acc_A_train, val_acc_A_train = train_A('Datasets\\train\\Train_A.csv')
+#print(acc_A_train)
+#print(val_acc_A_train)
 
 
 
